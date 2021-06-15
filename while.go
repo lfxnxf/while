cec3efed307a6be32a532266c78406f784f75813
“xuefeng” <xuefeng@soyoung.com>
2021-06-15 20:39:10 +0800

    第一次提交

---
+++ while.go
@@ -0,0 +1,41 @@
+package utils
+
+import "context"
+
+type While struct {
+	Ctx        context.Context
+	Cancel     context.CancelFunc
+	MaxWorkNum int
+}
+
+func NewWhile(maxWorkNum int) While {
+	ctx, cancel := context.WithCancel(context.Background())
+	return While{
+		Ctx:        ctx,
+		Cancel:     cancel,
+		MaxWorkNum: maxWorkNum,
+	}
+}
+
+func (w *While) For(f func()) {
+	var count int
+loop:
+	for {
+		f()
+		count++
+		if count >= w.MaxWorkNum {
+			break
+		}
+
+		select {
+		case <-w.Ctx.Done():
+			break loop
+		default:
+			continue
+		}
+	}
+}
+
+func (w *While) Break() {
+	w.Cancel()
+}
